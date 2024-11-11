2fa64b1a779ee6866cfd1e63a685bc08eccd5c7d
sbstndb <sebastiendubois4@gmail.com>
2024-11-11 14:17:27 -0500

    Type : xt::eval

--- src/complex.cpp
+++ src/complex.cpp
@@ -15,9 +15,9 @@ const int PS = 21 ; // pow size
 
 
 template <typename T = double>
-struct fma_op {
-    T operator()(const T& a, const T& b, const T& c) const {
-        return std::fma(a, b, c);
+struct complex_op {
+    T operator()(const T& a, const T& b, const T& c, const T& d, const T& e) const {
+        return a * b +  c * d * e;
     }
 };
 
@@ -33,20 +33,24 @@ void BM_RawSum(benchmark::State& state) {
 
     T* vec1 = static_cast<T*>(std::malloc(vector_size * sizeof(T)));
     T* vec2 = static_cast<T*>(std::malloc(vector_size * sizeof(T)));
+    T* vec3 = static_cast<T*>(std::malloc(vector_size * sizeof(T)));
+    T* vec4 = static_cast<T*>(std::malloc(vector_size * sizeof(T)));
+    
     T* result = static_cast<T*>(std::malloc(vector_size * sizeof(T)));
 
     // Initialize arrays
     for (int i = 0; i < vector_size; ++i) {
         vec1[i] = 1;
         vec2[i] = 2;
+	vec3[i] = 3 ; 
+	vec4[i] = 4 ; 
 	result[i] = 0;
     }
 
     for (auto _ : state) {
         // compute loop
         for (int i = 0; i < vector_size; ++i) {
-              result[i] = operation(a, vec1[i] , vec2[i]) ;
-		
+  	    result[i] = operation(a, vec1[i], vec2[i], vec3[i], vec4[i]) ; 
         }
 
         benchmark::DoNotOptimize(result); // compiler artifice 
@@ -55,6 +59,8 @@ void BM_RawSum(benchmark::State& state) {
 
     free(vec1) ;
     free(vec2) ;
+    free(vec3) ; 
+    free(vec4) ; 
     free(result) ;
 
     // report throughput
@@ -72,11 +78,15 @@ void BM_AlignedAllocSum(benchmark::State& state) {
     // Allocate aligned memory using std::aligned_alloc
     T* vec1 = static_cast<T*>(std::aligned_alloc(alignment, vector_size * sizeof(T)));
     T* vec2 = static_cast<T*>(std::aligned_alloc(alignment, vector_size * sizeof(T)));
+    T* vec3 = static_cast<T*>(std::aligned_alloc(alignment, vector_size * sizeof(T)));
+    T* vec4 = static_cast<T*>(std::aligned_alloc(alignment, vector_size * sizeof(T)));
     T* result = static_cast<T*>(std::aligned_alloc(alignment, vector_size * sizeof(T)));
     // Initialize arrays
     for (int i = 0; i < vector_size; ++i) {
         vec1[i] = 1;
         vec2[i] = 2;
+	vec3[i] = 3; 
+	vec4[i] = 4; 
 	result[i] = 0 ;
     }
 
@@ -86,8 +96,7 @@ void BM_AlignedAllocSum(benchmark::State& state) {
 
         // compute loop
         for (int i = 0; i < vector_size; ++i) {
-//              result[i] = operation(a, vec1[i] , vec2[i]) ;
-                result[i] = a * vec1[i] + vec2[i] ;
+              result[i] = operation(a, vec1[i] , vec2[i] , vec3[i] , vec4[i]) ;
 
         }
 
@@ -97,6 +106,8 @@ void BM_AlignedAllocSum(benchmark::State& state) {
     // Free aligned memory
     std::free(vec1);
     std::free(vec2);
+    std::free(vec3) ; 
+    std::free(vec4) ;
     std::free(result);
 
     state.SetItemsProcessed(state.iterations() * vector_size);
@@ -111,12 +122,14 @@ void BM_VectorSum(benchmark::State& state) {
 
     std::vector<T> vec1(vector_size, 1);
     std::vector<T> vec2(vector_size, 2);
+    std::vector<T> vec3(vector_size, 3);
+    std::vector<T> vec4(vector_size, 4);
     std::vector<T> result(vector_size, 0);
     for (auto _ : state) {
 
         // compute loop
         for (int i = 0; i < vector_size; ++i) {
-		result[i] = operation(a, vec1[i] , vec2[i]) ; 
+		result[i] = operation(a, vec1[i] , vec2[i], vec3[i] , vec4[i]) ; 
 	}
         benchmark::DoNotOptimize(result); // compiler artifice 
     }
@@ -133,16 +146,19 @@ void BM_XArraySum(benchmark::State& state) {
 
     xt::xarray<T> vec1 = xt::xarray<T>::from_shape({vector_size});
     xt::xarray<T> vec2 = xt::xarray<T>::from_shape({vector_size});
+    xt::xarray<T> vec3 = xt::xarray<T>::from_shape({vector_size});
+    xt::xarray<T> vec4 = xt::xarray<T>::from_shape({vector_size});
     xt::xarray<T> result = xt::xarray<T>::from_shape({vector_size});
-
     vec1.fill(1) ; 
     vec2.fill(2) ; 
+    vec3.fill(3) ; 
+    vec4.fill(4) ; 
     result.fill(0) ; 
 
     for (auto _ : state) {
 	// lot of constexpr because of xtensor itself
-	if constexpr(std::is_same_v<Op, fma_op<T>>){
-	        xt::noalias(result) = a * vec1 + vec2;
+	if constexpr(std::is_same_v<Op, complex_op<T>>){
+	        xt::noalias(result) = a * vec1 + vec2 * vec3 * vec4;
 	}
         benchmark::DoNotOptimize(result.data());
     }
@@ -158,16 +174,20 @@ void BM_XTensorSum(benchmark::State& state) {
 
     xt::xtensor<T, 1> vec1   = xt::xtensor<T,1>::from_shape({vector_size});
     xt::xtensor<T, 1> vec2   = xt::xtensor<T,1>::from_shape({vector_size});
+    xt::xtensor<T, 1> vec3   = xt::xtensor<T,1>::from_shape({vector_size});
+    xt::xtensor<T, 1> vec4   = xt::xtensor<T,1>::from_shape({vector_size});
     xt::xtensor<T, 1> result = xt::xtensor<T,1>::from_shape({vector_size});
 
     vec1.fill(1) ; 
     vec2.fill(2) ; 
+    vec3.fill(3) ; 
+    vec4.fill(4) ; 
     result.fill(0) ; 
 
     for (auto _ : state) {
 
-        if constexpr(std::is_same_v<Op, fma_op<T>>){
-                xt::noalias(result) = a * vec1 + vec2;
+        if constexpr(std::is_same_v<Op, complex_op<T>>){
+                xt::noalias(result) = a * vec1 + vec2 * vec3 * vec4 ;
         }
 
         benchmark::DoNotOptimize(result.data());
@@ -183,15 +203,18 @@ void BM_XTensorSumEval(benchmark::State& state) {
     T a = static_cast<T>(2.0) ;    
     xt::xtensor<T, 1> vec1   = xt::xtensor<T,1>::from_shape({vector_size});
     xt::xtensor<T, 1> vec2   = xt::xtensor<T,1>::from_shape({vector_size});
+    xt::xtensor<T, 1> vec3   = xt::xtensor<T,1>::from_shape({vector_size});
+    xt::xtensor<T, 1> vec4   = xt::xtensor<T,1>::from_shape({vector_size});    
     xt::xtensor<T, 1> result = xt::xtensor<T,1>::from_shape({vector_size});
-
     vec1.fill(1);
     vec2.fill(2);
+    vec3.fill(3) ; 
+    vec4.fill(4) ; 
     result.fill(0);
     for (auto _ : state) {
 
-        if constexpr(std::is_same_v<Op, fma_op<T>>){
-                xt::noalias(result) = xt::eval(a * vec1 + vec2);
+        if constexpr(std::is_same_v<Op, complex_op<T>>){
+                xt::noalias(result) = xt::eval(a * vec1 + vec2 * vec3 * vec4);
         }
 
         benchmark::DoNotOptimize(result.data());
@@ -203,25 +226,24 @@ void BM_XTensorSumEval(benchmark::State& state) {
 
 
 // Power of two rule
-BENCHMARK_TEMPLATE(BM_RawSum, int32_t,	fma_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
-BENCHMARK_TEMPLATE(BM_RawSum, float,	fma_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_RawSum, int32_t,	complex_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_RawSum, float,	complex_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 
-BENCHMARK_TEMPLATE(BM_AlignedAllocSum, int32_t,	fma_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
-BENCHMARK_TEMPLATE(BM_AlignedAllocSum, float,	fma_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_AlignedAllocSum, int32_t,	complex_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_AlignedAllocSum, float,	complex_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 
-BENCHMARK_TEMPLATE(BM_VectorSum, int32_t,	fma_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
-BENCHMARK_TEMPLATE(BM_VectorSum, float,		fma_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
-
+BENCHMARK_TEMPLATE(BM_VectorSum, int32_t,	complex_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_VectorSum, float,		complex_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 #ifdef XBENCHMARK_USE_XTENSOR
-BENCHMARK_TEMPLATE(BM_XArraySum, int32_t,	fma_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
-BENCHMARK_TEMPLATE(BM_XArraySum, float, 	fma_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_XArraySum, int32_t,	complex_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_XArraySum, float, 	complex_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 
-BENCHMARK_TEMPLATE(BM_XTensorSum, int32_t,	fma_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
-BENCHMARK_TEMPLATE(BM_XTensorSum, float,	fma_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_XTensorSum, int32_t,	complex_op<	int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_XTensorSum, float,	complex_op<	float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 
 
-BENCHMARK_TEMPLATE(BM_XTensorSumEval, int32_t,      fma_op<      int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
-BENCHMARK_TEMPLATE(BM_XTensorSumEval, float,        fma_op<      float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_XTensorSumEval, int32_t,      complex_op<      int32_t>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK_TEMPLATE(BM_XTensorSumEval, float,        complex_op<      float>)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 #endif
 
 
