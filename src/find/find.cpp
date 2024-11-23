a1c48b868ef50edf46dff77a169330f1cc989fb2
sbstndbs <sebastiendubois4@gmail.com>
2024-11-24 00:01:33 +0100

    Add binary search
    	-	Add std binary search in find benchmark

--- src/find/find.cpp
+++ src/find/find.cpp
@@ -71,6 +71,21 @@ void FIND_std_find(benchmark::State& state){
         free(vector);
 }
 
+void FIND_std_lower_bound(benchmark::State& state){
+        const int size = state.range(0) ;
+        int* vector = (int*) aligned_alloc(64, sizeof(int) * size) ;
+        int value = 1 ;
+        init_vector(vector, size, value, size-1);
+        int index ;
+        for (auto _ : state){
+                index = find_std_lower_bound(vector, size, value);
+                benchmark::DoNotOptimize(index);
+        }
+        state.SetItemsProcessed(state.iterations() * size);
+        free(vector);
+}
+
+
 
 void FIND_intrinsic(benchmark::State& state){
         int size = state.range(0) ;
@@ -97,6 +112,7 @@ BENCHMARK(FIND_naive)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 BENCHMARK(FIND_no_break)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 BENCHMARK(FIND_compare)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 BENCHMARK(FIND_std_find)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
+BENCHMARK(FIND_std_lower_bound)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 BENCHMARK(FIND_intrinsic)->RangeMultiplier(RM)->Range(MS << 0, 1 << PS);
 BENCHMARK_MAIN() ; 
 
